46e16e3d91dc6468332e003c053644f362931f3c
Faruhiz <jaus.kgg@gmail.com>
2024-02-23 20:55:43 +0700

    push finished lab to repo

--- w7/lab2_login/lib/pages/LoginPage.dart
+++ w7/lab2_login/lib/pages/LoginPage.dart
@@ -1,7 +1,14 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, library_private_types_in_public_api, use_key_in_widget_constructors, avoid_unnecessary_containers, avoid_print, slash_for_doc_comments, file_names
 
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+
+/**
+ * NOTE: I have to install form_builder_validators package otherwise I can't validate the form
+ * Installed dependency details: 
+ * - form_builder_validators: ^9.1.0 (https://pub.dev/packages/form_builder_validators)
+ */
 
 class LoginPage extends StatefulWidget {
   @override
@@ -16,36 +23,86 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         child: Center(
           child: SingleChildScrollView(
-              child: Column(
-            children: [
-              Image(
-                image: AssetImage('assets/profile.png'),
-                height: 100,
-              ),
-              Text('Login', style: TextStyle(fontSize: 20, color: Colors.blue)),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: FormBuilder(
-                  key: _formKey,
-                  initialValue: {'username': '', 'password': ''},
-                  child: Column(
-                    children: [
-                      FormBuilderTextField(
-                        name: 'username',
-                        decoration:
-                            InputDecoration(labelText: 'Email', filled: true),
-                        validator: FormBuilderValidators.compose([
-                          FormBuilderValidators.required(context,
-                              errorText: 'Please enter your email'),
-                          FormBuilderValidators.email(context),
-                        ]),
-                      ),
-                    ],
-                  ),
+            child: Column(
+              children: [
+                Image(
+                  image: AssetImage('assets/profile.png'),
+                  height: 100,
                 ),
-              ),
-            ],
-          )),
+                Text('Login ',
+                    style: TextStyle(fontSize: 20, color: Colors.blue)),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: FormBuilder(
+                    key: _formKey,
+                    initialValue: {'username': '', 'password': ''},
+                    child: Column(
+                      children: [
+                        FormBuilderTextField(
+                          name: 'username ',
+                          decoration: InputDecoration(
+                            labelText: 'Email ',
+                            filled: true,
+                          ),
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(
+                                errorText: 'please insert email '),
+                            FormBuilderValidators.email(),
+                          ]),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        SizedBox(height: 15),
+                        FormBuilderTextField(
+                          name: 'password ',
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'password',
+                            filled: true,
+                          ),
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(
+                                errorText: 'please insert password '),
+                            FormBuilderValidators.minLength(8,
+                                errorText: 'min length 8 character '),
+                          ]),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          child: MaterialButton(
+                            onPressed: () {
+                              _formKey.currentState!.save();
+                              if (_formKey.currentState!.validate()) {
+                                Navigator.pushNamed(context, '/launcher');
+                              } else {
+                                print(" validation failed ");
+                              }
+                            },
+                            child: Text(" Login ",
+                                style: TextStyle(color: Colors.blue)),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Row(
+                          children: [
+                            Expanded(
+                                child: MaterialButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, '/register');
+                              },
+                              child: Text('Register User ',
+                                  style: TextStyle(color: Colors.blue)),
+                            ))
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
